de6d47c85b4f73b1a80537dce369a44876fe715b
akshay.gore <a.gore@partner.samsung.com>
2021-01-13 23:09:41 -0800

    Add integrated test for checkAndGenerate function

--- JestPuppeteerTesting/util.test.js
+++ JestPuppeteerTesting/util.test.js
@@ -1,6 +1,6 @@
 const { generateText } = require("./util");
-
-test("should output the text", () => {
+// unit tests
+test("should output the text Akshay", () => {
   const text = generateText("Akshay", 29);
   expect(text).toBe("Akshay [29 years old]");
 });
@@ -8,4 +8,11 @@ test("should output the text", () => {
 test("should output type string", () => {
   const text = generateText("Akshay", 29);
   expect(typeof text).toBe("string");
+});
+
+//integrated tests
+
+test("should output the text Gore", () => {
+  const text = generateText("Gore", 29);
+  expect(text).toBe("Gore [29 years old]");
 });
